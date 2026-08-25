74d72e60004d03c301ca2ac13249b6c36c2dc3f8
o-yutaka <56465176+o-yutaka@users.noreply.github.com>
2026-08-25 15:49:23 +0900

    feat: type NEAR analytics events

--- mobile/lib/core/near_analytics.dart
+++ mobile/lib/core/near_analytics.dart
@@ -1,11 +1,10 @@
+enum NearEvent { appOpened, onboardingCompleted, nearbyViewed, profileViewed, chatStarted, messageSent, nowViewed, nowCreated, connectionRequested, connectionAccepted, blocked, reported, inviteOpened }
+
 class NearAnalytics {
   const NearAnalytics({this.sink = _noop});
-
   final void Function(String event, Map<String, Object?> properties) sink;
 
-  void track(String event, [Map<String, Object?> properties = const {}]) {
-    sink(event, properties);
-  }
+  void track(NearEvent event, [Map<String, Object?> properties = const {}]) => sink(event.name, properties);
 
   static void _noop(String event, Map<String, Object?> properties) {}
 }
